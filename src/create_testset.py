e1fc6d18d31080b1daf6add3edc8c5d6682f987d
Yeonji Jo <cherish22ee@gmail.com>
2025-07-12 08:03:09 +0900

    edited create_testset and added changing .txt file into .csv file (text, label)

--- src/create_testset.py
+++ src/create_testset.py
@@ -17,7 +17,29 @@ with open(input_file, "r", encoding="utf-8") as file:
                 continue
         
             # detect category
-            if list(line)[0] == "[" and list(line)[-1] == "]":
-                print(line)
-                current_label = ''.join(list(line)[1:-1])
+            # example : [category] -> category
+            if line[0] == "[" and line[-1] == "]":
+                current_label = ''.join(line[1:-1])
                 continue
+            
+            if line[0].isdigit() and "." in line:
+                try:
+                    # example : n. abuse speaking -> abuse speaking, current_label
+                    split_index = line.index(".")
+                    text = line[split_index+2:]
+                    print(text)
+                    data.append({
+                        "text": text,
+                        "label": current_label
+                    })
+                except:
+                    continue
+
+# save CSV file
+with open(output_file, "w", newline="", encoding="utf-8") as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=["text", "label"])
+    writer.writeheader()
+    for row in data:
+        writer.writerow(row)
+
+print(f"총 {len(data)}개 문장 저장됨 -> {output_file}")
