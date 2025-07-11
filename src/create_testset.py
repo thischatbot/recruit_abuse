7b5c5802c1ba58157a5c57b788c2279955514b82
Yeonji Jo <cherish22ee@gmail.com>
2025-07-12 07:38:35 +0900

    edited create_testset and added detecting category from .txt file

--- src/create_testset.py
+++ src/create_testset.py
@@ -1,17 +1,23 @@
 import csv
 
-#set file path
+# set file path
 input_file = "abuse_speaking.txt"
 output_file = "testset.csv"
 
-#list for saving results
+# list for saving results
 data = []
 
-#variable for searching current category name
+# variable for detecting category
 current_label = None
 
 with open(input_file, "r", encoding="utf-8") as file:
         for line in file:
             line = line.strip()
             if not line:
+                continue
+        
+            # detect category
+            if list(line)[0] == "[" and list(line)[-1] == "]":
+                print(line)
+                current_label = ''.join(list(line)[1:-1])
                 continue
