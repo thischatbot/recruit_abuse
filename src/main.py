738f5de17af1d66712238ea8fe750f26849efbcd
Yeonji Jo <cherish22ee@gmail.com>
2025-07-15 12:32:47 +0900

    added summarization prompt chain

--- src/main.py
+++ src/main.py
@@ -27,7 +27,20 @@ load_dotenv()
 api_key = os.getenv("SOLAR_API_KEY")
 chat = ChatUpstage(api_key=api_key)
 
-user_input = "여자는 연애를 하고 결혼을 하고 애를 낳아야 한다. 그렇지 않으면 같이 일 못하겠다."
+user_input = "면접관이 화장품 알레르기가 있는 저에게 \"여자가 화장을 해야지, 쌩얼 자신 있나.\"라고 면박을 줬습니다."
+# labeling 
+summarization_prompt = PromptTemplate.from_template(
+    """
+    다음 문장을 요약하세요.
+    "성차별", "나이차별", "모욕적 언행" 중 하나라도 해당되는 상황이 있으면 생략하지 마세요.
+    "성차별", "나이차별", "모욕적 언행" 중 해당되지 않은 것이 있으면 포함시키지 마세요.
+    주관적인 의견을 출력하지 마세요. 분류된 라벨을 출력하지 마세요.
+    문장 : {text}
+    """
+)
+summarization_chain = ({"text": RunnablePassthrough()} | summarization_prompt | chat | StrOutputParser())
+user_input = summarization_chain.invoke(user_input)
+print("요약된 문장 : " + user_input)
 #####
 # create vector DB
 
@@ -89,7 +102,8 @@ law_explain_prompt = PromptTemplate.from_template(
     설명:
 
     알맞은 법률 조항을 언급하여 다음 문장이 어떤 법에 위반되는지 분류하고 설명을 하세요.
-    "성차별", "연령차별", "모욕적 언행" 중에서 분류를 하세요.
+    객관적으로 법을 참고하여 "성차별", "연령차별", "모욕적 언행" 중에서 분류를 하세요.
+    법에 위반되지 않으면 분류하지 마세요.
     두개 이상 모두 해당되면 모두 포함시키세요.
     
     [관련 법]
